06451cdcb4b5bc41bad14b77c95b7bdc88247bd7
hzn <17302498739@163.com>
2026-03-14 09:38:33 +0800

    struct

--- 111.cpp
+++ 111.cpp
@@ -2,12 +2,37 @@
 #include<cmath>
 #include<algorithm>
 #include<string>
+#include<vector>
+
 using namespace std;
+struct stu
+{
+    string id;
+    int a;
+
+
+
+};
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int x;
     cin>>x;
-    cout<<"Celsius = "<<(x-32)*5/9;
+    vector<stu> students(x+1);
+    while(x--){
+        string id;
+        int index,a;
+        cin>>id>>index>>a;
+        students[index].id=id;
+        students[index].a=a;
+    }
+    cin>>x;
+    while(x--)
+    {
+        int index;
+        cin>>index;
+        cout<<students[index].id<<" "<<students[index].a<<endl;
+    }
     return 0;
 }
