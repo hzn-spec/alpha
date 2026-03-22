54d983681bb921dbef49633272f1537f0b7d8189
hzn <17302498739@163.com>
2026-03-22 18:53:48 +0800

    模板

--- 111.cpp
+++ 111.cpp
@@ -1,28 +1,12 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
-using ll = long long;
-void solve() {
-    int n;
-    ll sum = 0;
-    cin >> n;
-    vector<ll> a(n);
-    for (int i = 0; i < n; ++i)
-        cin >> a[i], sum += a[i];
-    ll p = sum > 0 ? (sum + n - 1) / n : sum / n, chg = 0;
-    for (int i = 0; i < n; ++i) {
-        if (a[i] < p)
-            chg += p - a[i];
-    }
-    cout << chg << endl;
-}
-int main() {
+int main(){
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int t;
-    cin >> t;
-    while (t--) {
-        solve();
-    }
+    cin.tie(0);
+    
+
+
+
+
     return 0;
 }
